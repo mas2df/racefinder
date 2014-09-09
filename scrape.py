a2b1cc675c51a92d679bd013dff3f333e2e813e6
Michael Santamaria <michael.santamaria@gmail.com>
2014-09-08 22:51:09 -0400

    Iterate through rows; need to strip characters and get actual data

--- scrape.py
+++ scrape.py
@@ -1,6 +1,31 @@
 import requests
 import bs4
+import logging
+import re
 
-response = requests.get('http://runningintheusa.com/Race/List.aspx?Rank=All&State=VA&Page=1')
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+site_url = 'http://runningintheusa.com/Race/List.aspx?Rank=All&State=VA&Page=1'
+data_row_class_regex = "MenuGridView(Alternating)?Row"
+
+race_list = []
+header_list = ["skip", "date", "race", "location"]
+
+response = requests.get(site_url)
 soup = bs4.BeautifulSoup(response.text)
-links = soup.select('.MenuGridViewHeader')
+rows = soup.find_all(class_=re.compile(data_row_class_regex))
+for row in rows:
+    race_dict = {}
+    for index, td in enumerate(row.find_all("td")):
+        if index < len(header_list) and header_list[index] != "skip":
+
+            # TODO pull out specific info - strip newlines
+            raw = td.text
+            raw = raw[1:-1]
+            race_dict[header_list[index]] = raw
+
+    # Append the race_dict to the list
+    race_list.append(race_dict)
+
+logger.info("race_list: " + race_list)
