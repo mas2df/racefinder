49f667e5370f8a96f7ce7da2749ad9e1f932bc77
Michael Santamaria <michael.santamaria@gmail.com>
2014-09-11 20:35:42 -0400

    Parsing out date, race name, and types

--- scrape.py
+++ scrape.py
@@ -10,22 +10,26 @@ site_url = 'http://runningintheusa.com/Race/List.aspx?Rank=All&State=VA&Page=1'
 data_row_class_regex = "MenuGridView(Alternating)?Row"
 
 race_list = []
-header_list = ["skip", "date", "race", "location"]
+header_list = ["skip", "date", "race", "location", "link"]
 
 response = requests.get(site_url)
 soup = bs4.BeautifulSoup(response.text)
 rows = soup.find_all(class_=re.compile(data_row_class_regex))
 for row in rows:
     race_dict = {}
-    for index, td in enumerate(row.find_all("td")):
-        if index < len(header_list) and header_list[index] != "skip":
+    td_list = row.find_all("td")
 
-            # TODO pull out specific info - strip newlines
-            raw = td.text
-            raw = raw[1:-1]
-            race_dict[header_list[index]] = raw
+    date = td_list[1].get_text().replace("\n", "")
+    race_name = td_list[2].div.find_all("a")[1].string
+    race_type_list = td_list[2].div.find_all("div")[1].string.split(", ")
+
+    race_dict = {
+        "date": date,
+        "race_name": race_name,
+        "type": race_type_list
+    }
 
     # Append the race_dict to the list
     race_list.append(race_dict)
 
-logger.info("race_list: " + race_list)
+logger.info("race_list: " + str(race_list))
