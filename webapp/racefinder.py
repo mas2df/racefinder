93756428da5cde0e2cf16aac9a76d61834a4afab
Michael Santamaria <mike@thehumangeo.com>
2014-09-14 20:29:23 -0400

    Added 404 page

--- webapp/racefinder.py
+++ webapp/racefinder.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
