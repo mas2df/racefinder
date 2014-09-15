c2d9f8ffe9bb4ad57776955f709d2d4e4e42034b
Michael Santamaria <mike@thehumangeo.com>
2014-09-14 20:41:39 -0400

    Load up entries and provide them to the template

--- webapp/racefinder.py
+++ webapp/racefinder.py
@@ -1,9 +1,15 @@
+import json
 from flask import Flask, render_template
 app = Flask(__name__)
 
+output_contents = []
+with open('../output.txt', 'r') as data_file:
+    output_contents = data_file.readlines()
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+
+    return render_template('home.html', races=output_contents)
 
 @app.errorhandler(404)
 def page_not_found(error):
