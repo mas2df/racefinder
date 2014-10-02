dcc79b1ef0290a4d7a786cf67dba72e68f3a923f
Michael Santamaria <mike@thehumangeo.com>
2014-10-02 16:19:56 -0400

    Race types are displayed in a list; Checking items goes to dummy function; Need to figure out how to filter elements based on checkbox

--- README.md
+++ README.md
@@ -5,28 +5,21 @@ Application for pulling race data and displaying it geospatially and temporally.
 
 Scraping
 <ul>
-    <li>Look up flyers: http://www.runningintheusa.com/Race/View.aspx?RaceID=90694</li>
-    <li>Collect rest of the states</li>
-    <li>Figure out out how to get specific locations</li>
-    <li>Things to collect:</li>
-    <ul>
-        <li>Run</li>
-        <li>Bike</li>
-        <li>Triathlon</li>
-        <li>Obstacle course racing (OCR)</li>  
-    </ul>
-    
-</ul>
+* Look up flyers: http://www.runningintheusa.com/Race/View.aspx?RaceID=90694
+* Collect rest of the states
+* Figure out out how to get specific locations
+* Things to collect:
+** Run
+** Bike
+** Triathlon
+** Obstacle course racing (OCR)  
 
 Webapp
-<ul>
-    <li>styles</li>
-    <li>filters</li>
-    <ul>
-        <li>run, bike, triathlon, etc.</li>
-        <li>distance</li>
-    </ul>
-    <li>timeline filter</li>
-    <li>facets</li>
-    <li>elasticsearch</li>
-</ul>
+* styles
+* filters
+** run, bike, triathlon, etc.
+** distance
+
+* timeline filter
+* facets
+* elasticsearch
--- webapp/static/js/racefinder.js
+++ webapp/static/js/racefinder.js
@@ -3,6 +3,7 @@
 var racefinder = {
 
     encoded_race_data: "",
+    race_type_set: {},
 
     map: null,
     initial_lat: 39.0,
@@ -25,15 +26,49 @@ var racefinder = {
 
         var markers = new L.MarkerClusterGroup();
 
+        // Iterate over races
         var race_list = this.decodeJSON(this.encoded_race_data);
         for (i in race_list) {
-            var race = L.marker([race_list[i].location.lat, race_list[i].location.lon]);
-            race.bindPopup("<ul><li>" + race_list[i].date + "<li>" + race_list[i].name + "<li>" + race_list[i].race_type + "<li><a href=\"" + race_list[i].race_site_url + "\" target=\"_blank\">link</a></ul>");
-            markers.addLayer(race);
+
+            // Create a marker for each race_marker
+            var race_marker = L.marker([race_list[i].location.lat, race_list[i].location.lon]);
+            race_marker.bindPopup("<ul><li>" + race_list[i].date + "<li>" + race_list[i].name + "<li>" + race_list[i].race_type + "<li><a href=\"" + race_list[i].race_site_url + "\" target=\"_blank\">link</a></ul>");
+            markers.addLayer(race_marker);
+
+            // Add the race_marker types to a set
+            var race_type_list = race_list[i].race_type;
+            for (j in race_type_list) {
+                var blah = race_type_list[j];
+                if (!(race_type_list[j] in this.race_type_set)) {
+                    this.race_type_set[race_type_list[j]] = true;
+                }
+            }
         }
+
+        this.addRaceFilters();
 
         this.map.addLayer(markers);
 
+    },
+
+    // Adds race types to the filters
+    addRaceFilters: function() {
+        var me = this;
+        var ul = $("<ul>");
+
+        var keys = Object.keys(this.race_type_set);
+        $.each(keys, function(i) {
+            var input = $("<input>", {type: 'checkbox', value: keys[i], checked: "true"});
+            input.on("click", me.applyFilter);
+            var li = $("<li/>").append(input).append(keys[i]).appendTo(ul);
+        });
+
+        $("#race-filter").append(ul);
+    },
+
+    applyFilter: function(el){
+        var val = this.value;
+        debugger;
     },
 
     decodeJSON: function (encodedJSON) {
